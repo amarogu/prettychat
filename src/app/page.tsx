75d236a9999174c63b4700e78f15f5a38592e4eb
Gustavo <gumarinho090306@gmail.com>
2023-12-14 23:23:55 -0300

    Add GlobalStateProvider to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import {Sidebar} from './Sidebar'
+import { GlobalStateProvider } from './GlobalStateContext'
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4">
-      <Sidebar />
-    </main>
+    <GlobalStateProvider>
+      <main className="min-h-screen p-4">
+        <Sidebar />
+      </main>
+    </GlobalStateProvider>
   )
 }
