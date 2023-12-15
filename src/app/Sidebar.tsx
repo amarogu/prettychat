d0948865d5ca29dbb24a4e5bc608d8a631e0e5cd
Gustavo <gumarinho090306@gmail.com>
2023-12-15 18:59:08 -0300

    Add logout functionality and close button to Sidebar component

--- src/app/Sidebar.tsx
+++ src/app/Sidebar.tsx
@@ -2,28 +2,33 @@ import React, { useEffect } from 'react';
 import ChatComponent from "./Chat"
 import { Create } from "@mui/icons-material"
 import { useGlobalState } from "./GlobalStateContext"
-import { getUser } from "./Networking/user"
+import { getUser, logout } from "./Networking/user"
 import { getChats } from "./Networking/chats"
 import { Account } from "./Account"
 import Searchbar from "./Searchbar"
+import CloseIcon from '@mui/icons-material/Close';
 
 export function Sidebar() {
-    const {chats, setChats, setUser, setIsLoggedIn} = useGlobalState(); // Add this line
+    const {chats, setChats, setUser, setIsLoggedIn, isLoggedIn} = useGlobalState(); // Add this line
 
     useEffect(() => {
-        getChats().then(chats => setChats(chats)); // Add this block
+        if (isLoggedIn) {
+            getChats().then(chats => setChats(chats));
+        } // Add this block
     }, []); // Empty dependencies array means this effect runs once on mount and not on updates
 
     useEffect(() => {
-        getUser().then(user => {
-            console.log(user)
-            setUser(user)
-            setIsLoggedIn(true)
-        }); // Add this block
+        if (isLoggedIn) {
+            getUser().then(user => {
+                console.log(user)
+                setUser(user)
+                setIsLoggedIn(true)
+            });
+        } // Add this block
     }, []);
 
     return (
-        <section className="max-w-xs flex gap-3 flex-col relative">
+        <section className="max-w-xs flex gap-3 flex-col">
             <div className="flex justify-between items-center">
                 <h1 className="text-xl font-einaBold">Chats</h1>
                 <Create className="text-sm" />
@@ -33,6 +38,11 @@ export function Sidebar() {
                 chats.map((chat) => <ChatComponent chat={chat} />)
             }
             <Account />
+            <button onClick={() => {
+                logout()
+            }}>
+                <CloseIcon className='text-sm absolute right-0 top-0 m-4' />
+            </button>
         </section>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,12 @@
 import Image from 'next/image'
 import {Sidebar} from './Sidebar'
 import { GlobalStateProvider } from './GlobalStateContext'
-import CloseIcon from '@mui/icons-material/Close';
 
 export default function Home() {
   return (
     <GlobalStateProvider>
       <main className="min-h-screen p-4">
         <Sidebar />
-        <button>
-          <CloseIcon className='text-sm absolute right-0 top-0 m-4' />
-        </button>
       </main>
     </GlobalStateProvider>
   )
