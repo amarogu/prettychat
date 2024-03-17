cb6b9951b40d2d07f78e7bb2ed5991616e105cee
Gustavo <gumarinho090306@gmail.com>
2024-03-17 19:11:38 -0300

    Remove unused components and update Home page

--- src/app/Searchbar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Searchbar() {
-    return (
-        <div>
-            <input type="text" placeholder="Search" className="bg-gray border border-body-dark-emphasized text-body-dark-emphasized rounded py-2 px-3 w-full outline-none" />
-        </div>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-'use client'
-import Image from 'next/image'
-import {Sidebar} from './Sidebar'
-import { GlobalStateProvider } from './GlobalStateContext'
-
-export default function Home() {
+export default async function Home() {
   return (
-    <GlobalStateProvider>
-      <main className="min-h-screen p-4">
-        <Sidebar />
-      </main>
-    </GlobalStateProvider>
+    <main className="min-h-screen p-4">
+        
+    </main>
   )
 }
