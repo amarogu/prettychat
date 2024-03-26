45342b09d3b64d7c11a5b8f0e9c20c3095c20904
Gustavo <gumarinho090306@gmail.com>
2024-03-26 18:11:59 -0300

    Add Btn component to Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
+import Btn from "../Btn";
 import Input from "../Input";
 import Popup from "../Popup";
 
 export default function Dashboard() {
     return (
-        <main className="h-screen p-8">
+        <main className="h-screen flex items-center justify-center p-8">
             <Popup title="Great, you are in!" message="Remember. everything here is self-hosted, so you're fine inserting your API key.">
                 <Input placeholder="API Key" />
+                <Btn content="Continue" />
             </Popup>
         </main>
     )
