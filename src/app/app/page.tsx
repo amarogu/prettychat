ded54b5a727039a1e63ae8ff21378bea4a912bb5
Gustavo <gumarinho090306@gmail.com>
2024-03-26 21:24:30 -0300

    Remove networking and welcome components

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -2,7 +2,7 @@ import Btn from "../Btn";
 import Input from "../Input";
 import Popup from "../Popup";
 
-export default function Dashboard() {
+export default function App() {
     return (
         <main className="h-screen flex items-center justify-center p-8">
             <Popup title="Great, you are in!" message="Remember. everything here is self-hosted, so you're fine inserting your API key.">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Welcome from "./Welcome";
+import Link from "next/link"
+import Btn from "./Btn"
+import Popup from "./Popup"
 
 export default async function Home() {
   return (
     <main className="h-screen p-4">
         <div className="h-full flex items-center justify-center">
-          <Welcome title='Welcome! ✨' type="input" btn="Continue" input="Server URL" message={['PrettyChat was designed to run on-device, so you will need to host its server and database yourself. Pretty simple stuff, though. Check the docs at ', <a key={0} className="underline" href="https://github.com/amarogu/prettychat" target="_blank">https://github.com/amarogu/prettychat.</a>, ' In order to continue, please insert the url that points to your self-managed server.']} />
+          <Popup title='Welcome! ✨' message={['PrettyChat was designed to run on-device, so you will need to host its server and database yourself. Pretty simple stuff, though. Check the docs at ', <a key={0} className="underline" href="https://github.com/amarogu/prettychat" target="_blank">https://github.com/amarogu/prettychat.</a>, ' In order to continue, please insert the url that points to your self-managed server.']}>
+            <Link href="/app" />
+          </Popup>
         </div>
     </main>
   )
