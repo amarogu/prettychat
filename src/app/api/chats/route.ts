05a41f619eab2e03f2ff5f0b0ade3522fb3dc994
Gustavo <gumarinho090306@gmail.com>
2024-03-30 19:29:31 -0300

    Add chat search functionality

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,12 +1,14 @@
 import { NextRequest } from "next/server";
 import connectDb from "../../../../connect";
+import Chat from "../../../../models/Chat";
 
 export default async function POST(req: NextRequest) {
     try {
         const reqBody = await req.json() as { name: string };
         await connectDb();
-        
+        const chats = await Chat.find({name: reqBody.name});
+        return Response.json(chats);
     } catch (err: any) {
-
+        return Response.json({message: 'An error occurred', error: err});
     }
 }
