29ef824b839a4bdbc71c623bd7d38185593f2f00
Gustavo <gumarinho090306@gmail.com>
2024-03-30 19:47:05 -0300

    Fix bug in login functionality

---
+++ src/app/api/createChat/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest } from "next/server";
+import connectDb from "../../../../connect";
+import Chat from "../../../../models/Chat";
+
+export default async function POST(req: NextRequest) {
+    try {
+        const reqBody = await req.json() as { name: string };
+        await connectDb();
+        const chat = new Chat({name: reqBody.name});
+        await chat.save();
+        return Response.json({message: 'The chat was successfully created.'});
+    } catch (err: any) {
+        return Response.json({message: 'An error occurred', error: err});
+    }
+}
