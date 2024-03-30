6524a96d16848c62f8f685cfa100eb511220e5ef
Gustavo <gumarinho090306@gmail.com>
2024-03-30 13:08:44 -0300

    Delete unused middleware and API route, refactor chat page

--- src/app/app/chat/page.tsx
+++ src/app/app/chat/page.tsx
@@ -4,23 +4,13 @@ import Btn from "@/app/Btn";
 import { useSession, signOut } from "next-auth/react";
 
 export default function Chat() {
-
     const {data: session} = useSession();
-
-    if (session) {
-        return (
-            <main>
-                <h1>{session?.user?.name}</h1>
-                <Btn content="logout" onClick={() => {
-                    signOut();
-                }} />
-            </main>
-        )
-    } else {
-        return (
-            <Popup title="Unauthorized" message="You must be logged in to access this page.">
-                <Btn content="Login page" href="/app/login" />
-            </Popup>
-        )
-    }
+    return (
+        <main>
+            <h1>{session?.user?.name}</h1>
+            <Btn content="logout" onClick={() => {
+                signOut();
+            }} />
+        </main>
+    )
 }
