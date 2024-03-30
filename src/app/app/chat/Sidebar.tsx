200f47b356391cb390c44127f24cf930d0e03f91
Gustavo <gumarinho090306@gmail.com>
2024-03-30 18:28:51 -0300

    Add session user name to Sidebar component

--- src/app/app/chat/Sidebar.tsx
+++ src/app/app/chat/Sidebar.tsx
@@ -1,6 +1,10 @@
 import Input from "@/app/Input";
+import { useSession } from "next-auth/react";
 
 export default function Sidebar() {
+
+    const {data: session} = useSession();
+
     return (
         <aside className="h-full flex flex-col gap-8 w-2/3 max-w-xs">
             <div className="flex justify-between items-center">
@@ -8,6 +12,12 @@ export default function Sidebar() {
                 <p className="text-xl">+</p>
             </div>
             <Input placeholder="Search" className="outline-gray bg-gray" />
+            <div className="h-full"></div>
+            <div className="flex">
+                <div className="flex flex-col gap-2">
+                    <p>{session?.user?.name}</p>
+                </div>
+            </div>
         </aside>
     )
 }
