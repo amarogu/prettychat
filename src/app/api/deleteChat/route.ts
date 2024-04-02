bb62512fd47402671c545d70206cfc22b3211d28
Gustavo <gumarinho090306@gmail.com>
2024-04-02 18:25:12 -0300

    Refactor deleteChat route to handle chat deletion and creation

--- src/app/api/deleteChat/route.ts
+++ src/app/api/deleteChat/route.ts
@@ -12,11 +12,19 @@ export async function POST(req: NextRequest) {
         await connectDb();
         const reqBody = await req.json() as { _id: string };
         const chat = await Chat.findById({ _id: reqBody._id});
+        const chats = await Chat.find();
         if (session) {
             if (chat) {
                 if (session.user?.name === chat.name) {
-                    await chat.deleteOne();
-                    return Response.json({message: 'The chat was successfully deleted.'});
+                    if (chats.length > 1) {
+                        await chat.deleteOne();
+                        return Response.json({message: 'The chat was successfully deleted.'});
+                    } else {
+                        await chat.deleteOne();
+                        const newChat = new Chat({name: session.user?.name});
+                        await newChat.save();
+                        return Response.json({message: 'The chat was successfully deleted. A new chat was created.'});
+                    }
                 } else {
                     return new Response(new Blob([JSON.stringify({message: 'You are not authorized to delete this chat'})], {type: 'application/json'}), {status: 403});
                 }
