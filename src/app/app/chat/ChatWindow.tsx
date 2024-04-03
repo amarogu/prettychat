b6ef1987ba1555c66065b76137736e9af2be69cf
Gustavo <gumarinho090306@gmail.com>
2024-04-03 19:34:02 -0300

    Refactor ChatWindow component to display chat messages

--- src/app/app/chat/ChatWindow.tsx
+++ src/app/app/chat/ChatWindow.tsx
@@ -13,9 +13,6 @@ interface ChatWindowProps {
 
 export default function ChatWindow({chat, getChat}: ChatWindowProps) {
 
-    const [localChat, setLocalChat] = useState<IChat | null>(chat);
-    const [useLocal, setUseLocal] = useState<boolean>(true);
-
     const {
         completion,
         input,
@@ -31,58 +28,35 @@ export default function ChatWindow({chat, getChat}: ChatWindowProps) {
         },
         onFinish: () => {
             if (chat?._id) {
-                setUseLocal(false);
                 getChat(chat._id);
             }
         }
     });
 
-    useEffect(() => {
-        if (completion) {
-
+    const shouldDisplayChat = () => {
+        if (chat?.messages.length !== 0) {
+            return chat?.messages.map((message) => (
+                <div key={message._id} className={`${message.sender === 'user' ? 'self-end' : 'self-start'} bg-gray px-4 py-2`}>
+                    <p>{message.content}</p>
+                    <p className="text-body-dark">{message.sender === 'system' ? 'System' : chat.name}</p>
+                </div>
+            ));
+        } else {
+            return (
+                <div className="grow flex flex-col justify-center items-center">
+                    <h1 className="text-2xl font-einaBold">New Chat</h1>
+                    <p className="w-1/4 text-center">To begin chatting, start typing in the input field below.</p>
+                </div>
+            );
         }
-    }, [completion])
+    }
 
     return (
         <section className="flex h-full grow gap-4 flex-col">
             <div className="grow flex flex-col gap-4 overflow-y-scroll justify-start">
-                {!useLocal ?
-                    (chat?.messages.length !== 0 ? chat?.messages.map((message) => (
-                        <div key={message._id} className={`${message.sender === 'user' ? 'self-end' : 'self-start'} bg-gray px-4 py-2`}>
-                            <p>{message.content}</p>
-                            <p className="text-body-dark">{message.sender === 'system' ? 'System' : chat.name}</p>
-                        </div>
-                    )) : (
-                        <div className="grow flex flex-col justify-center items-center">
-                            <h1 className="text-2xl font-einaBold">New Chat</h1>
-                            <p className="w-1/4 text-center">To begin chatting, start typing in the input field below.</p>
-                        </div>
-                    )) : (localChat?.messages.length !== 0 ? localChat?.messages.map((message) => (
-                        <div key={message._id} className={`${message.sender === 'user' ? 'self-end' : 'self-start'} bg-gray px-4 py-2`}>
-                            <p>{message.content}</p>
-                            <p className="text-body-dark">{message.sender === 'system' ? 'System' : localChat.name}</p>
-                        </div>
-                    )) : (
-                        <div className="grow flex flex-col justify-center items-center">
-                            <h1 className="text-2xl font-einaBold">New Chat</h1>
-                            <p className="w-1/4 text-center">To begin chatting, start typing in the input field below.</p>
-                        </div>
-                    ))
-                }
+                {shouldDisplayChat()}
             </div>
             <form className="flex gap-2" onSubmit={(e) => {
-                setUseLocal(true);
-                for (let i = 0; i < 2; i++) {
-                    const msg: IMessage = {
-                        _id: uuidv4(),
-                        content: i === 0 ? input : '',
-                        sender: i === 0 ? 'user' : 'system',
-                        updatedAt: new Date().toISOString(),
-                        createdAt: new Date().toISOString(),
-                        __v: 0
-                    };
-                    localChat?.messages.push(msg);
-                }
                 handleSubmit(e);
             }}>
                 <Input value={input} onChange={handleInputChange} id="message-input" placeholder="Type a message" className="bg-gray grow outline-gray" />
