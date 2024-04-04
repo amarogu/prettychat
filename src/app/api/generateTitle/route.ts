e65160f001505b7beed69c148850f8ff8aa4be14
Gustavo <gumarinho090306@gmail.com>
2024-04-04 16:28:58 -0300

    Add OpenAI integration and message formatting

--- src/app/api/generateTitle/route.ts
+++ src/app/api/generateTitle/route.ts
@@ -1,7 +1,9 @@
 import { getServerSession } from "next-auth";
 import { NextRequest } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/route";
-import { Chat } from "../../../../models/Chat";
+import { Chat, IChat } from "../../../../models/Chat";
+import OpenAI from "openai";
+import { User } from "../../../../models/User";
 
 export async function POST(req: NextRequest) {
     const session = await getServerSession(authOptions);
@@ -11,7 +13,19 @@ export async function POST(req: NextRequest) {
             const chat = await Chat.findById(reqBody.chatId);
             if (chat) {
                 if (session.user.name === chat.name) {
-                    
+                    const populatedChat = await chat.populate('messages') as unknown as IChat;
+                    const formattedMsgs = populatedChat.messages.map(msg => {
+                        return {
+                            role: msg.role,
+                            content: msg.content
+                        }
+                    });
+                    const user = await User.findOne({name: session.user.name});
+                    const openai = new OpenAI({apiKey: user?.apiKey});
+                    const completion = await openai.chat.completions.create({
+                        model: 'gpt-3.5-turbo',
+                        messages: [{role: 'system', content: ''}]
+                    })
                 } else {
                     return new Response(new Blob([JSON.stringify({message: 'You are not authorized to access or modify this chat'})], {type: 'application/json'}), {status: 403});
                 }
