dcae00a4f91830d835311697372346b6299f4ed7
Gustavo <gumarinho090306@gmail.com>
2024-04-05 19:38:03 -0300

    Update Input component to use react-textarea-autosize package and add support for icon

--- src/app/Input.tsx
+++ src/app/Input.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 
 interface InputProps {
@@ -7,13 +8,25 @@ interface InputProps {
     className?: string;
     onChange?: (e: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
     value?: string;
+    icon?: ReactNode;
 }
 
-export default function Input({placeholder, id, type, className, onChange, value}: InputProps) {
+export default function Input({placeholder, id, type, className, onChange, value, icon}: InputProps) {
     return (
         <>
             {
-                type === 'textarea' ? <TextareaAutosize maxRows={20} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 resize-none ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} value={value} onChange={onChange} id={id} placeholder={placeholder} /> : <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
+                type === 'textarea' 
+                ? 
+                <TextareaAutosize maxRows={20} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 resize-none ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} value={value} onChange={onChange} id={id} placeholder={placeholder} /> 
+                : (icon 
+                    ? 
+                    <div className='flex gap-2 outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200 px-4 py-2'>
+                        {icon}
+                        <p>{placeholder}</p>
+                    </div> 
+                    : 
+                    <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
+                )
             }
         </>
     )
