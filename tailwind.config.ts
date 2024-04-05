5768da76a873926b56bb12be570aee47e61b5218
Gustavo <gumarinho090306@gmail.com>
2024-04-05 15:38:13 -0300

    Update tailwind.config.ts with new color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,17 @@ const config: Config = {
         'text-200': '#5c5c5c',
         'bg-100': '#F2F2F2',
         'bg-200': '#e8e8e8',
-        'bg-300': '#bfbfbf'
+        'bg-300': '#bfbfbf',
+        'dark-primary-100': '#333333',
+        'dark-primary-200': '#5c5c5c',
+        'dark-primary-300': '#b9b9b9',
+        'dark-accent-100': '#666666',
+        'dark-accent-200': '#f7f7f7',
+        'dark-text-100': '#FFFFFF',
+        'dark-text-200': '#e0e0e0',
+        'dark-bg-100': '#1A1A1A',
+        'dark-bg-200': '#292929',
+        'dark-bg-300': '#404040'
       },
       fontFamily: {
         einaReg: ['var(--font-eina-reg)'],
