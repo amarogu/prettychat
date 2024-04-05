d78642d0743fb10fe23e4a080ee9707c6648bf7a
Gustavo <gumarinho090306@gmail.com>
2024-04-05 16:21:42 -0300

    Refactor Input component to support textarea type

--- src/app/Input.tsx
+++ src/app/Input.tsx
@@ -9,6 +9,10 @@ interface InputProps {
 
 export default function Input({placeholder, id, type, className, onChange, value}: InputProps) {
     return (
-        <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
+        <>
+            {
+                type === 'textarea' ? <textarea value={value} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} /> : <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
+            }
+        </>
     )
 }
