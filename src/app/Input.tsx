9939ccdd388ebe4c3861fe806939f3c368248364
Gustavo <gumarinho090306@gmail.com>
2024-04-05 16:24:51 -0300

    Update Input component to support textarea type in Input.tsx

--- src/app/Input.tsx
+++ src/app/Input.tsx
@@ -3,7 +3,7 @@ interface InputProps {
     id?: string;
     type?: string;
     className?: string;
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange?: (e: React.ChangeEvent<HTMLTextAreaElement> | React.ChangeEvent<HTMLInputElement>) => void;
     value?: string;
 }
 
@@ -11,7 +11,7 @@ export default function Input({placeholder, id, type, className, onChange, value
     return (
         <>
             {
-                type === 'textarea' ? <textarea value={value} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} /> : <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
+                type === 'textarea' ? <textarea value={value} onChange={onChange} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} /> : <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
             }
         </>
     )
