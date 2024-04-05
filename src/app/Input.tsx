8846b51fbaf3c2232a65889bdbdff821adcf0c26
Gustavo <gumarinho090306@gmail.com>
2024-04-05 16:35:21 -0300

    Update Input component to use react-textarea-autosize package

--- src/app/Input.tsx
+++ src/app/Input.tsx
@@ -1,3 +1,5 @@
+import TextareaAutosize from 'react-textarea-autosize';
+
 interface InputProps {
     placeholder: string;
     id?: string;
@@ -11,7 +13,7 @@ export default function Input({placeholder, id, type, className, onChange, value
     return (
         <>
             {
-                type === 'textarea' ? <textarea rows={1} value={value} onChange={onChange} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 resize-none ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} /> : <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
+                type === 'textarea' ? <TextareaAutosize maxRows={20} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 resize-none ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} value={value} onChange={onChange} id={id} placeholder={placeholder} /> : <input onChange={onChange} value={value} type={type ?? 'text'} id={id} placeholder={placeholder} className={`rounded-sm focus:outline outline-offset-2 placeholder-text-100 dark:placeholder-dark-text-100 ${className ? className : 'outline-bg-300 dark:outline-dark-bg-300 bg-bg-200 dark:bg-dark-bg-200'} px-4 py-2`} />
             }
         </>
     )
