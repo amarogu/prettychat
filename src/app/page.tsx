b89c8e305d77eec1450f9ba4c80c2ff5ccc0bca6
Gustavo <gumarinho090306@gmail.com>
2024-04-06 17:20:49 -0300

    Initial release (0.1.1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <main className="h-screen p-4">
         <div className="h-full flex items-center justify-center">
-          <Popup title='Welcome! ✨' message={['PrettyChat was designed to run on-device, so you will need to host its server and database yourself. Pretty simple stuff, though. Check the docs at ', <a key={0} className="underline" href="https://github.com/amarogu/prettychat" target="_blank">https://github.com/amarogu/prettychat.</a>, ' In order to continue, please insert the url that points to your self-managed server.']}>
+          <Popup title='Welcome! ✨' message={['PrettyChat was designed to run on-device, so you will need to host its server and database yourself. Pretty simple stuff, though. Check the docs at ', <a key={0} className="underline" href="https://github.com/amarogu/prettychat" target="_blank">https://github.com/amarogu/prettychat.</a>]}>
             <Btn content="Register your key" href="/app/register" />
             <Btn content="Login" href="/app/login" />
           </Popup>
